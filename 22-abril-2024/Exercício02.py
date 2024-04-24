1a769d59a1c96179ab983bbc4d268c8eed685e9b
Rafael Letnski Soares <86269885+rflsoares@users.noreply.github.com>
2024-04-24 20:11:56 -0300

    Exercício02 corrigido

--- 22-abril-2024/Exercício02.py
+++ 22-abril-2024/Exercício02.py
@@ -1,18 +1,25 @@
-def idade(i,c):
-    if (i>18):
-        print("Por favor escolha a sua categoria")
-    elif (i<18):
-        print("Desculpe, mas você não pode escolher uma categoria, menor de idade")
-    if (c=="A"):
-        print("Categoria selecionada com sucesso!")
-    elif (c=="B"):          
-             
+def categorias(i, c, t, f):
+    if (i>=18):
+        if c == 'A' or c == 'B':
+            return True
+        elif c == 'C' and t >= 1 and f <= 1:
+            return True
+        elif c == 'D' and i >= 21 and t <= 2 and f <= 1:
+            return True
+        elif c == 'E' and i >= 21 and t >= 1 and f <= 1:
+            return True
+        return False          
 
-def dados():
-    x=float(input("Digite a sua idade"))
-    y=float(input("Digite a sua categoria"))
-    return x,y
+def main():
+    i = int(input("Digite sua idade: "))
+    c = input("Digite sua categoria atual (A, B, C, D, E): ")
+    t = int(input("Tempo de habilitação (em anos): "))
+    f = int(input("Número de infrações médias nos últimos 12 meses: "))
 
-if __name__=="__main__":
-    =dados()    
-    nota()
+    if categorias(i, c, t, f):
+        print("Você está elegível para selecionar esta categoria da CNH.")
+    else:
+        print("Você não está elegível para selecionar esta categoria da CNH.")
+
+if __name__ == "__main__":
+    main()
